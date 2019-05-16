6384efb9111db3b0b6088a5b90bc4d6858113b4f
manager <jefferson99soares@gmail.com>
2019-05-15 22:12:56 -0300

    Enviando projeto de CardView

---
+++ app/src/main/java/br/com/etecia/appcardview/Configuracoes_Activity.java
@@ -0,0 +1,21 @@
+package br.com.etecia.appcardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class Configuracoes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuracoes_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
